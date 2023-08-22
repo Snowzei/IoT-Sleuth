a00716b28abdec7140c51dfa91db8891543fe26c
Joe Watson <68339502+Snowzei@users.noreply.github.com>
2023-08-22 23:45:20 +0100

    cleanup for the pcap branch

--- Slueth/Model_Utils.py
+++ Slueth/Model_Utils.py
@@ -1,81 +1,34 @@
-from sklearn.preprocessing import LabelEncoder
-import pandas as pd
+from numpy import array, ndarray
+from numpy.random import shuffle
 
-# Convert hex strings to integers
-def convert_hex_to_int(value):
-    if isinstance(value, str):
-        return int(value, 16)
-    return value
+def load_data_to_trainable_dataset(x: list, y: list) -> tuple[ndarray, ndarray, ndarray, ndarray]:
+  """
+  This function takes two normal lists of the same length, shuffles and splits them into train and test sets, converts them into numpy arrays and returns them as x_train, x_test, y_train and y_test.
 
-def preprocess_csv(csv_file, shuffle = True):
-    # Read the CSV file using pandas
-    df = pd.read_csv(csv_file)
+  Parameters:
+  x (list): A list of features
+  y (list): A list of labels
 
-    df['Checksum'] = df['Checksum'].apply(convert_hex_to_int)
-    df['Options'] = df['Options'].apply(convert_hex_to_int)
+  Returns:
+  x_train (ndarray): A numpy array of the features for training
+  x_test (ndarray): A numpy array of the features for testing
+  y_train (ndarray): A numpy array of the labels for training
+  y_test (ndarray): A numpy array of the labels for testing
+  """
+  # Assuming x and y are normal lists of the same length
+  # Shuffle and split x and y into train and test sets
+  data = list(zip(x, y)) # Combine x and y into pairs
+  shuffle(data) # Shuffle the data randomly
+  train_data = data[:int(0.8 * len(data))] # Take 80% of the data for training
+  test_data = data[int(0.8 * len(data)):] # Take 20% of the data for testing
 
-    # Convert categorical values to label-encoded integers
-    label_encoder = LabelEncoder()
-    df['Protocol'] = label_encoder.fit_transform(df['Protocol'])
-    df['Flags'] = label_encoder.fit_transform(df['Flags'])
+  # Convert the train and test sets into numpy arrays
+  x_train, y_train = zip(*train_data) # Unzip the train data into x and y
+  x_test, y_test = zip(*test_data) # Unzip the test data into x and y
+  x_train = array(x_train) # Convert x_train to numpy array
+  y_train = array(y_train) # Convert y_train to numpy array
+  x_test = array(x_test) # Convert x_test to numpy array
+  y_test = array(y_test) # Convert y_test to numpy array
 
-    # Handle NaN values by filling them with a specific value or imputing
-    # For example, filling NaN with 0
-    df.fillna(0, inplace=True)
-
-    # Remove the "Info" column
-    df = df.drop("Info", axis=1)
-
-    # Shuffle the dataset
-    if shuffle:
-        df = df.sample(frac=1).reset_index(drop=True)
-
-    # Split features and labels
-    features = df.drop('Label', axis=1)
-    labels = df['Label']
-
-    # Convert features and labels to NumPy arrays with appropriate data types
-    features_array = features.to_numpy().astype(float)  # Convert to float to handle mixed types
-    labels_array = labels.to_numpy()
-
-    #split the features and labels further into training and testing data
-    split_index = int(len(features_array) * 0.8)  # Index to split at
-
-    train_features = features_array[:split_index]  # First 80% of the array
-    test_features = features_array[split_index:] # Last 20% of the array
-
-    train_labels = labels_array[:split_index]  # First 80% of the array
-    test_labels = labels_array[split_index:] # Last 20% of the array
-
-    # print(train_features[:10])
-    # print(train_labels[:10])
-    # print(test_features[:10])
-    # print(test_labels[:10])
-
-    # Convert the NumPy arrays to TensorFlow tensors and create the dataset
-    # dataset = tf.data.Dataset.from_tensor_slices((features_array, labels_array))
-
-    return train_features, train_labels, test_features, test_labels
-
-'''
-def dataset_into_features_and_labels(dataset):
-    # Assuming dataset is a TensorFlow dataset with features and labels
-
-    # Split the TensorFlow dataset into features and labels
-    features_dataset = dataset.map(lambda x, y: x)
-    labels_dataset = dataset.map(lambda x, y: y)
-
-    # Convert features and labels datasets into arrays
-    features_array = np.array(list(features_dataset.as_numpy_iterator()))
-    labels_array = np.array(list(labels_dataset.as_numpy_iterator()))
-    return features_array, labels_array
-
------------------------
-def split_dataset(dataset):
-    # Split the dataset into train, validation, and test sets
-    train_size = int(0.8 * len(dataset))
-
-    train_dataset = dataset.take(train_size)
-    test_dataset = dataset.skip(train_size)
-    return train_dataset, test_dataset
-'''
+  # Return the numpy arrays
+  return x_train, x_test, y_train, y_test
--- Slueth/Models.py
+++ Slueth/Models.py
@@ -1,163 +1,130 @@
-from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.utils import shuffle
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+import tensorflow as tf
+import numpy as np
+from tensorflow_probability.python.distributions import Dirichlet, Beta
 
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.optimizers import Adam
+def train_model(model: tf.keras.Model, x_train: np.ndarray, y_train: np.ndarray,
+                x_val: np.ndarray, y_val: np.ndarray, epochs: int) -> tf.keras.callbacks.History:
+    """Trains a model on the given data.
 
-from numpy import unique
+    Args:
+        model (tf.keras.Model): A TensorFlow model object.
+        x_train (np.ndarray): The training data array.
+        y_train (np.ndarray): The training labels array.
+        x_val (np.ndarray): The validation data array.
+        y_val (np.ndarray): The validation labels array.
+        epochs (int): The number of epochs to train.
 
-def evaluate_model(model, test_features, test_labels):
-    accuracy = model.score(test_features, test_labels)
-    print("Test accuracy:", accuracy)
+    Returns:
+        tf.keras.callbacks.History: A history object that contains the loss and accuracy values for each epoch.
+    """
+    # Fit the model to the training data
+    history = model.fit(x_train, y_train,
+                        validation_data=(x_val, y_val),
+                        epochs=epochs)
 
-def create_multinomial_naive_bayes(features, labels):
-    model = MultinomialNB()
-    model.fit(features, labels)
+    # Return the history object
+    return history
+
+'''
+-------------------------------
+Classification Models:
+- Naive bayes
+- Support vector machine
+- K Nearest Neighbour
+- Decision tree/ Random Forest
+-------------------------------
+'''
+
+def create_naive_bayes_model(num_classes: int) -> tf.keras.Model:
+    """Creates a naive bayes model for predicting device types.
+
+    Args:
+        int: num_classes: The number of different devices, this is a parameter for ease of use later.
+
+    Returns:
+        tf.keras.Model: A TensorFlow model object.
+    """
+    # define the prior distribution for the class probabilities
+    pi = Dirichlet(concentration=tf.ones(num_classes))
+
+    # define the prior distribution for the feature probabilities
+    theta = Beta(concentration0=tf.ones([num_classes, 10]), concentration1=tf.ones([num_classes, 10]))
+
+    # define the posterior distribution for the class and feature probabilities using variational inference
+    q_pi = Dirichlet(concentration=tf.Variable(tf.ones(num_classes)))
+    q_theta = Beta(concentration0=tf.Variable(tf.ones([num_classes, 10])), concentration1=tf.Variable(tf.ones([num_classes, 10])))
+
+    # create a simple naive bayes model with TensorFlow
+    model = tf.keras.Model(inputs=[], outputs=[q_pi, q_theta])
+    
     return model
 
-def create_gaussian_naive_bayes(features, labels):
-    model = GaussianNB()
-    model.fit(features, labels)
+def create_SVM_model(num_classes: int) -> tf.keras.Model:
+    """Creates a Support vector machine model for predicting device types.
+
+    Returns:
+        tf.keras.Model: A TensorFlow model object.
+    """
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(num_classes, activation='linear')
+    ])
+    model.compile(loss=tf.keras.losses.CategoricalHinge(),
+                  optimizer=tf.keras.optimizers.SGD())
+    
     return model
 
-def create_complement_naive_bayes(features, labels):
-    model = ComplementNB()
-    model.fit(features, labels)
+def create_KNN_model(num_classes: int) -> tf.keras.Model:
+    """Creates a K Nearest Neighbour model for predicting device types.
+
+    Returns:
+        tf.keras.Model: A TensorFlow model object.
+    """
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam())
+    
     return model
 
-def create_svc(features, labels):
-    model = SVC()
-    model.fit(features, labels)
-    return model
+def create_random_forest_model(num_classes: int) -> tf.keras.Model:
+    """Creates a Random Forest model for predicting device types.
 
-def create_knn(features, labels):
-    model = KNeighborsClassifier()
-    model.fit(features, labels)
-    return model
+    Returns:
+        tf.keras.Model: A TensorFlow model object.
+    """
+    model = tf.keras.experimental.RandomForest(tf.keras.Input(shape=(None,)),
+                                               num_trees=10,
+                                               max_depth=5,
+                                               num_classes=num_classes)
 
-def create_decision_tree(features, labels):
-    model = DecisionTreeClassifier()
-    model.fit(features, labels)
-    return model
-
-def create_random_forest(features, labels):
-    model = RandomForestClassifier()
-    model.fit(features, labels)
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam())
+    
     return model
 
 '''
-Neural Networks
-----------------------------------------------------------------------------------------------------------------------------------
+-------------------------------
+Regression Models:
+- Multi-Layer Perceptron
+-------------------------------
 '''
-def evaluate_neural_network(trained_model, test_features, test_labels, normalise=False, encoding=False):
-    if normalise:
-        scaler = StandardScaler()
-        test_features = scaler.fit_transform(test_features)
-    if encoding:
-        label_encoder = LabelEncoder()
-        test_labels = label_encoder.fit_transform(test_labels)
-    loss, accuracy = trained_model.evaluate(test_features, test_labels)
-    print("Test Loss:", loss)
-    print("Test Accuracy:", accuracy)
-    return
 
-def create_and_train_neural_network_mk_1(train_features, train_labels, num_epochs=10):
-    # Get the num_classes from the data
-    num_classes = len(unique(train_labels))
-    # Create a neural network model
-    model = Sequential([
-        Dense(64, activation='relu', input_shape=(train_features.shape[1],)),
-        Dense(32, activation='relu'),
-        Dense(num_classes, activation='softmax')
-    ])
-    # Compile the model
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    # Train the model
-    model.fit(train_features, train_labels, epochs=num_epochs, verbose=1)
-    return model
+def create_MLP_model() -> tf.keras.Model:
+    """Creates a multi-layer perceptron model for predicting device types.
 
-def create_and_train_neural_network_mk_2(train_features, train_labels, num_epochs=10):
-    # Encode labels
-    label_encoder = LabelEncoder()
-    encoded_labels = label_encoder.fit_transform(train_labels)
-    # Get the num_classes from the data
-    num_classes = len(label_encoder.classes_)
-    # Create a neural network model
-    model = Sequential([
-        Dense(128, activation='relu', input_shape=(train_features.shape[1],)),
-        Dropout(0.2),
-        Dense(64, activation='relu'),
-        Dropout(0.2),
-        Dense(num_classes, activation='softmax')
+    Returns:
+        tf.keras.Model: A TensorFlow model object.
+    """
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1)
     ])
-    # Compile the model
-    optimizer = Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    # Train the model
-    model.fit(train_features, encoded_labels, epochs=num_epochs, verbose=1)
-    return model
 
-def create_and_train_neural_network_mk_3(train_features, train_labels, num_epochs=10):
-    # Normalise features
-    scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(train_features)
-    # Get the num_classes from the data
-    num_classes = len(unique(train_labels))
-    # Create a neural network model
-    model = Sequential([
-        Dense(256, activation='relu', input_shape=(scaled_features.shape[1],)),
-        BatchNormalization(),
-        Dropout(0.5),
-        Dense(128, activation='relu'),
-        BatchNormalization(),
-        Dropout(0.3),
-        Dense(64, activation='relu'),
-        BatchNormalization(),
-        Dense(num_classes, activation='softmax')
-    ])
-    # Compile the model
-    optimizer = Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    # Train the model
-    model.fit(scaled_features, train_labels, epochs=num_epochs, verbose=1)
-    return model
-
-def create_and_train_neural_network_mk_4(train_features, train_labels, num_epochs=10):
-    # Normalise features
-    scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(train_features)
-    # Encode labels
-    label_encoder = LabelEncoder()
-    encoded_labels = label_encoder.fit_transform(train_labels)
-    # Get the num_classes from the data
-    num_classes = len(label_encoder.classes_)
-    # Create a neural network model
-    model = Sequential([
-        Dense(256, activation='relu', input_shape=(scaled_features.shape[1],)),
-        BatchNormalization(),
-        Dropout(0.5),
-        Dense(128, activation='relu'),
-        BatchNormalization(),
-        Dropout(0.3),
-        Dense(64, activation='elu'),
-        BatchNormalization(),
-        Dropout(0.2),
-        Dense(32, activation='selu'),
-        BatchNormalization(),
-        Dense(num_classes, activation='softmax')
-    ])
-    # Compile the model
-    optimizer = Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    # Train the model
-    model.fit(scaled_features, encoded_labels, epochs=num_epochs, verbose=1)
+    model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                  optimizer=tf.keras.optimizers.Adam())
+    
     return model
