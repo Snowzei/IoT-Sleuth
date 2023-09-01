2bd7fbc3e6bc66b2e3cec77cb0fc684c62b8dfd2
Joe Watson <68339502+Snowzei@users.noreply.github.com>
2023-09-01 18:13:58 +0100

    we boolin, 62ish% accuracy but it works and looks good doing it

--- Slueth/Model_Utils.py
+++ Slueth/Model_Utils.py
@@ -2,56 +2,72 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 
 # Convert hex strings to integers
-def convert_hex_to_int(value):
+def convert_hex_to_int(value:str)->int:
+    '''
+    Convert a hexadecimal string to an integer.
+
+    Args:
+        value (str): The hexadecimal string to be converted.
+
+    Returns:
+        int: The integer representation of the hexadecimal value.
+
+    This function takes a hexadecimal string as input and returns the corresponding
+    integer representation. If the input is not a valid hexadecimal string, it
+    returns the input value as is. Use this function to convert hexadecimal values
+    to integers for further processing.
+    '''
     if isinstance(value, str):
         return int(value, 16)
     return value
 
-def preprocess_csv(csv_file, shuffle = True):
+def preprocess_csv(csv_file:str, shuffle:bool = True)->tuple:
+    '''
+    Preprocesses a CSV file containing IoT network data for machine learning.
+
+    Args:
+        csv_file (str): The path to the CSV file containing IoT network data.
+        shuffle (bool, optional): Whether to shuffle the dataset. Default is True.
+
+    Returns:
+        tuple: A tuple containing the following NumPy arrays:
+            - train_features (ndarray): Features for training data.
+            - train_labels (ndarray): Labels for training data.
+            - test_features (ndarray): Features for testing data.
+            - test_labels (ndarray): Labels for testing data.
+
+    This function reads the CSV file, performs data preprocessing tasks including
+    converting hex values, label encoding, handling NaN values, and optionally shuffling
+    the dataset. It then splits the data into training and testing sets, returning
+    NumPy arrays for further machine learning tasks.
+    '''
     # Read the CSV file using pandas
     df = pd.read_csv(csv_file)
-
     df['Checksum'] = df['Checksum'].apply(convert_hex_to_int)
     df['Options'] = df['Options'].apply(convert_hex_to_int)
-
     # Convert categorical values to label-encoded integers
     label_encoder = LabelEncoder()
     df['Protocol'] = label_encoder.fit_transform(df['Protocol'])
     df['Flags'] = label_encoder.fit_transform(df['Flags'])
-
-    # Handle NaN values by filling them with a specific value or imputing
-    # For example, filling NaN with 0
+    # Handle NaN values by filling them with a specific value
     df.fillna(0, inplace=True)
-
     # Remove the "Info" column
     df = df.drop("Info", axis=1)
-
     # Shuffle the dataset
     if shuffle:
         df = df.sample(frac=1).reset_index(drop=True)
-
     # Split features and labels
     features = df.drop('Label', axis=1)
     labels = df['Label']
-
     # Convert features and labels to NumPy arrays with appropriate data types
     features_array = features.to_numpy().astype(float)  # Convert to float to handle mixed types
     labels_array = labels.to_numpy()
-
     #split the features and labels further into training and testing data
     split_index = int(len(features_array) * 0.8)  # Index to split at
-
     train_features = features_array[:split_index]  # First 80% of the array
     test_features = features_array[split_index:] # Last 20% of the array
-
     train_labels = labels_array[:split_index]  # First 80% of the array
     test_labels = labels_array[split_index:] # Last 20% of the array
-
-    # print(train_features[:10])
-    # print(train_labels[:10])
-    # print(test_features[:10])
-    # print(test_labels[:10])
-
     # Convert the NumPy arrays to TensorFlow tensors and create the dataset
     # dataset = tf.data.Dataset.from_tensor_slices((features_array, labels_array))
 
--- Slueth/Models.py
+++ Slueth/Models.py
@@ -1,64 +1,184 @@
+# Import sklearn classes/functions
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.utils import shuffle
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.impute import SimpleImputer
 
+# Import tensorflow classes/functions
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 
-from numpy import unique
+# Import numpy features
+from numpy import unique, ndarray
 
-def evaluate_model(model, test_features, test_labels):
-    accuracy = model.score(test_features, test_labels)
+'''
+Classifiers
+----------------------------------------------------------------------------------------------------------------------------------
+'''
+
+def evaluate_model(model: any, test_features: ndarray, test_labels:ndarray, scaling_and_processing:bool=False):
+    '''
+    Evaluates a model.
+
+    Parameters:
+        model (any): The trained model from the sklearn library. 
+        test_features (ndarray): Input features as a 2D NumPy array.
+        test_labels (ndarray): Target labels as a 1D NumPy array.
+        scaling_and_processing (bool, optional): Whether to apply scaling and processing to test features.
+    '''
+    if scaling_and_processing:
+        # Preprocess the features
+        scaler = StandardScaler()
+        imputer = SimpleImputer(strategy='mean')
+        # Scale and impute the features
+        scaled_features = scaler.fit_transform(test_features)
+        preprocessed_features = imputer.fit_transform(scaled_features)
+        accuracy = model.score(preprocessed_features, test_labels)
+    else:
+        accuracy = model.score(test_features, test_labels)
     print("Test accuracy:", accuracy)
 
-def create_multinomial_naive_bayes(features, labels):
+def create_multinomial_naive_bayes(features: ndarray, labels: ndarray) -> MultinomialNB:
+    '''
+    Creates and trains a Multinomial Naive Bayes model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        MultinomialNB: Trained Multinomial Naive Bayes model.
+    '''
     model = MultinomialNB()
     model.fit(features, labels)
     return model
 
-def create_gaussian_naive_bayes(features, labels):
+def create_gaussian_naive_bayes(features: ndarray, labels: ndarray) -> GaussianNB:
+    '''
+    Creates and trains a Gaussian Naive Bayes model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        GaussianNB: Trained Gaussian Naive Bayes model.
+    '''
     model = GaussianNB()
     model.fit(features, labels)
     return model
 
-def create_complement_naive_bayes(features, labels):
+def create_complement_naive_bayes(features: ndarray, labels: ndarray) -> ComplementNB:
+    '''
+    Creates and trains a Complement Naive Bayes model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        ComplementNB: Trained Complement Naive Bayes model.
+    '''
     model = ComplementNB()
     model.fit(features, labels)
     return model
 
-def create_svc(features, labels):
+def create_svc(features: ndarray, labels: ndarray) -> SVC:
+    '''
+    Creates and trains a C-Support Vector Classification model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        SVC: Trained C-Support Vector Classification model.
+    '''
     model = SVC()
     model.fit(features, labels)
     return model
 
-def create_knn(features, labels):
+def create_knn(features: ndarray, labels: ndarray) -> KNeighborsClassifier:
+    '''
+    Creates and trains a K-Nearest Neighbors model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        KNeighborsClassifier: Trained K-Nearest Neighbors model.
+    '''
     model = KNeighborsClassifier()
     model.fit(features, labels)
     return model
 
-def create_decision_tree(features, labels):
+def create_decision_tree(features: ndarray, labels: ndarray) -> DecisionTreeClassifier:
+    '''
+    Creates and trains a Decision Tree Classifier.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        DecisionTreeClassifier: Trained Decision Tree classifier model.
+    '''
+    # Preprocess the features
+    scaler = StandardScaler()
+    imputer = SimpleImputer(strategy='mean')
+    # Scale and impute the features
+    scaled_features = scaler.fit_transform(features)
+    preprocessed_features = imputer.fit_transform(scaled_features)
+    # Create and train the model
     model = DecisionTreeClassifier()
-    model.fit(features, labels)
+    model.fit(preprocessed_features, labels)
     return model
 
-def create_random_forest(features, labels):
+def create_random_forest(features: ndarray, labels: ndarray) -> RandomForestClassifier:
+    '''
+    Creates and trains a Random Forest classifier.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        RandomForestClassifier: Trained Random Forest classifier model.
+    '''
+    # Preprocess the features
+    scaler = StandardScaler()
+    imputer = SimpleImputer(strategy='mean')
+    # Scale and impute the features
+    scaled_features = scaler.fit_transform(features)
+    preprocessed_features = imputer.fit_transform(scaled_features)
+    # Create and train the model
     model = RandomForestClassifier()
-    model.fit(features, labels)
+    model.fit(preprocessed_features, labels)
     return model
 
 '''
 Neural Networks
 ----------------------------------------------------------------------------------------------------------------------------------
 '''
-def evaluate_neural_network(trained_model, test_features, test_labels, normalise=False, encoding=False):
+
+def evaluate_neural_network(trained_model:Sequential, test_features:ndarray, test_labels:ndarray, normalise:bool=False, encoding:bool=False):
+    '''
+    Evaluates a neural network using the test dataset.
+
+    Parameters:
+        trained_model (Sequential): The trained model.
+        test_features (ndarray): Testing input features as a 2D NumPy array.
+        test_labels (ndarray): Testing target labels as a 1D NumPy array.
+        normalise (bool, optional): Whether to apply encoding to test features. 
+        encoding (bool, optional): Whether to apply normalising to test labels.
+    '''    
     if normalise:
         scaler = StandardScaler()
         test_features = scaler.fit_transform(test_features)
@@ -70,7 +190,17 @@ def evaluate_neural_network(trained_model, test_features, test_labels, normalise
     print("Test Accuracy:", accuracy)
     return
 
-def create_neural_network_mk_1(train_features, train_labels, num_epochs=10):
+def create_neural_network_mk_1(train_features:ndarray, train_labels:ndarray, num_epochs:int=10) -> Sequential:
+    '''
+    Creates and trains a neural network using the tensorflow library. This is version 1 of the model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        Sequential: Trained neural network model.
+    '''
     # Get the num_classes from the data
     num_classes = len(unique(train_labels))
     # Create a neural network model
@@ -85,7 +215,17 @@ def create_neural_network_mk_1(train_features, train_labels, num_epochs=10):
     model.fit(train_features, train_labels, epochs=num_epochs, verbose=1)
     return model
 
-def create_neural_network_mk_2(train_features, train_labels, num_epochs=10):
+def create_neural_network_mk_2(train_features:ndarray, train_labels:ndarray, num_epochs:int=10) -> Sequential:
+    '''
+    Creates and trains a neural network using the tensorflow library. This is version 2 of the model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        Sequential: Trained neural network model.
+    '''
     # Encode labels
     label_encoder = LabelEncoder()
     encoded_labels = label_encoder.fit_transform(train_labels)
@@ -106,7 +246,17 @@ def create_neural_network_mk_2(train_features, train_labels, num_epochs=10):
     model.fit(train_features, encoded_labels, epochs=num_epochs, verbose=1)
     return model
 
-def create_neural_network_mk_3(train_features, train_labels, num_epochs=10):
+def create_neural_network_mk_3(train_features:ndarray, train_labels:ndarray, num_epochs:int=10) -> Sequential:
+    '''
+    Creates and trains a neural network using the tensorflow library. This is version 3 of the model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        Sequential: Trained neural network model.
+    '''
     # Normalise features
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(train_features)
@@ -131,7 +281,17 @@ def create_neural_network_mk_3(train_features, train_labels, num_epochs=10):
     model.fit(scaled_features, train_labels, epochs=num_epochs, verbose=1)
     return model
 
-def create_neural_network_mk_4(train_features, train_labels, num_epochs=10):
+def create_neural_network_mk_4(train_features:ndarray, train_labels:ndarray, num_epochs:int=10) -> Sequential:
+    '''
+    Creates and trains a neural network using the tensorflow library. This is version 4 of the model.
+
+    Parameters:
+        features (ndarray): Input features as a 2D NumPy array.
+        labels (ndarray): Target labels as a 1D NumPy array.
+
+    Returns:
+        Sequential: Trained neural network model.
+    '''
     # Normalise features
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(train_features)
